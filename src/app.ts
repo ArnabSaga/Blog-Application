e61b1dc6c5d76e37ed5e514575feac54a9870f50
ArnabSaga <arnabdey15091@gmail.com>
2025-12-29 21:04:00 +0600

    Better-Auth setup & Implement the Email & Password Authentication

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { Application, Request, Response } from "express";
 
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
+
 import { PostRouter } from "./modules/post/post.route";
 
 const app: Application = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
